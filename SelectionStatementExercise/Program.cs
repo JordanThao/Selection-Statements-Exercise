91591105686ffcffae82b051347e88644ef51841
Jordan Thao <Jordan.thao23@gmail.com>
2024-09-24 11:15:30 -0600

    Number Guessing Game

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random r = new Random();
+            int favNumber = r.Next(1, 10);
+            //int favNumber = 5;
+            int userInput;
+            var counter = 0;
+
+            do
+            {
+                Console.WriteLine("Pick a number between 1 - 1000");
+                userInput = int.Parse(Console.ReadLine());
+
+                if (userInput < favNumber)
+                {
+                    Console.WriteLine($"{userInput} is too low.");
+                }
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine($"{userInput} is too high.");
+                }
+                else
+                {
+                    Console.WriteLine($"You got it, {userInput} is the number!");
+                }
+
+                counter++;
+                Console.WriteLine($"You have guess {counter} times.");
+
+            } while (userInput != favNumber);
         }
     }
 }
